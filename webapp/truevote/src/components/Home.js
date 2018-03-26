9a471a7623941ca03690b0f711a0eab1ba4b630d
Bruce Crane <samcrane8@gmail.com>
2018-03-26 16:44:00 -0400

    added router
    some other stuff I can’t remember

---
+++ webapp/truevote/src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css';
+
+const Home = () => (
+	<div>
+		<div class="stretched row">
+		  <div class="column">
+	      <div class="ui card segment">
+		 			VOTE!!
+		 		</div>
+		  </div>
+		  <div class="column">
+	     <div class="ui card segment">
+		 			VOTE!!
+		 		</div>
+		  </div>
+		  <div class="column">
+		      <div class="ui card segment">
+			 			VOTE!!
+			 		</div>
+		  </div>
+	  </div>
+  </div>
+)
+
+export default Home
---
+++ webapp/truevote/src/components/Main.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './Home'
+import Landing from './Landing'
+
+// The Main component renders one of the three provided
+// Routes (provided that one matches). Both the /roster
+// and /schedule routes will match any pathname that starts
+// with /roster or /schedule. The / route will only match
+// when the pathname is exactly the string "/"
+const Main = () => (
+  <main>
+  	<BrowserRouter>
+	    <Switch>
+	      <Route exact path='/' component={Home}/>
+	      <Route path='/landing' component={Landing}/>
+	    </Switch>
+    </BrowserRouter>
+  </main>
+)
+
+export default Main
