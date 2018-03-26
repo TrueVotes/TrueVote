55535570e9c33e9f68770db3ef614ab7d6a715cb
Bruce Crane <samcrane8@gmail.com>
2018-03-26 14:24:24 -0400

    testing
    - now actually can import these functions into the react project.
    - now we’re cooking with fire.

--- truevote-engine/truevote.js
+++ truevote-engine/truevote.js
@@ -1,14 +1,11 @@
-var Mam = require('../lib/mam.node.js')
 var IOTA = require('iota.lib.js')
 var iota = new IOTA({ provider: `http://eugene.iota.community:14265` })
 
-// Initialise MAM State - PUBLIC
-var mamState = Mam.init(iota)
 
-export function send_vote() {
+exports.send_vote = function() {
   console.log('Beginning send_vote function.')
 }
 
-export function get_poll_results() {
+exports.get_poll_results = function() {
   console.log('Beginning get_poll_results function.')
 }
