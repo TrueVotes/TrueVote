eaca4426bddeffda5f24b40714b4bc4e9476429c
Bruce Crane <samcrane8@gmail.com>
2018-03-28 16:57:11 -0400

    new poll updates

--- webapp/src/components/NewPoll.js
+++ webapp/src/components/NewPoll.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Form, Radio, Grid, Card, Icon, Image, Dropdown } from 'semantic-ui-react'
+import {Button, Form, Radio, Grid, Card, Icon, Image, Dropdown } from 'semantic-ui-react'
 
 
 class NewPoll extends Component {
@@ -7,17 +7,16 @@ class NewPoll extends Component {
 	state = {}
   handleChange = (e, { value }) => this.setState({ value })
 
+  poll_operators = [{'title': 'dude', 'type': 'NUMERIC'}]
+
 	render() {
 		return (
 			<div>
-			  <div class="ui three column grid">
+			  <div class="ui column grid">
 			    <div class="row">
-			      <div class="column"></div>
-			      <div class="column"></div>
 			      <div class="column"></div>
 			    </div>
 			    <div class="row">
-			      <div class="column"></div>
 			      <div class="column">
 			        <h2 class="ui purple image header">
 			          <div class="content">
@@ -35,42 +34,56 @@ class NewPoll extends Component {
 			            <div class="field">
 			              <div class="ui left icon input">
 			                <i class="person icon"></i>
-			                <input type="username" name="first_name" placeholder="Description"/>
+			                <input type="username" name="first_name" placeholder="Destination Account"/>
 			              </div>
 			            </div>
 			            <div class="field">
 			              <div class="ui left icon input">
 			                <i class="person icon"></i>
-			                <input type="username" name="first_name" placeholder="License ID"/>
+			                <input type="username" name="first_name" placeholder="Start Time"/>
 			              </div>
 			            </div>
 			            <div class="field">
 			              <div class="ui left icon input">
 			                <i class="person icon"></i>
-			                <input type="username" name="first_name" placeholder="Vote"/>
+			                <input type="username" name="first_name" placeholder="End Time"/>
 			              </div>
 			            </div>
-			            <div class="ui row grid centered">
-			              <Form.Field style={{margin:20}}>
-			                <Radio
-			                  label='Mugabe'
-			                  name='radioGroup'
-			                  value='this'
-			                  checked={this.state.value === 'this'}
-			                  onChange={this.handleChange}
-			                />
-			              </Form.Field>
-			              <Form.Field style={{margin:20}}>
-			                <Radio
-			                  label='Incumbent'
-			                  name='radioGroup'
-			                  value='that'
-			                  checked={this.state.value === 'that'}
-			                  onChange={this.handleChange}
-			                />
-			              </Form.Field>
+			            <div class="field">
+			              <div class="ui left icon input">
+			                <i class="person icon"></i>
+			                <input type="username" name="first_name" placeholder="Min Responses"/>
+			              </div>
 			            </div>
-			            <div class="ui fluid large purple submit button">Login</div>
+			            <div class="field">
+			              <div class="ui left icon input">
+			                <i class="person icon"></i>
+			                <input type="username" name="first_name" placeholder="Max Responses"/>
+			              </div>
+			            </div>
+			            {this.poll_operators.map(option => 
+			            	<div class="inline fields"> 
+			            		<label>{option["title"]}</label>
+			            		<label>{option["type"]}</label>
+			            		<Button icon='close'>
+								      	
+								      </Button>
+			            	</div>
+			            )} 
+			            <div class="inline fields">
+								    <label>Setting:</label>
+								    <div class="ui left icon input">
+			                <i class="person icon"></i>
+			                <input type="username" name="first_name" placeholder="Title"/>
+			                <input type="username" name="first_name" placeholder="Type"/>
+			              </div>
+								    <div class="field">
+								      <Button>
+								      	Add
+								      </Button>
+								    </div>
+			            </div>
+			            <div class="ui fluid large purple submit button">Generate</div>
 			          </div>
 
 			          <div class="ui error message"></div>
@@ -78,11 +91,8 @@ class NewPoll extends Component {
 			        </form>
 			      </div>
 			    </div>
-			    <div class="column"></div>
 			  </div>
 			  <div class="row">
-			    <div class="column"></div>
-			    <div class="column"></div>
 			    <div class="column"></div>
 			  </div>
 		  </div>
